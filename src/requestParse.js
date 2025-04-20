6a5273bc0af798feb2dd409ff4e9f5f57f1582b5
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2025-04-20 23:14:03 +0300

    Fix empty search

--- src/requestParse.js
+++ src/requestParse.js
@@ -1,7 +1,8 @@
 function requestParse(url) {
   const [path, search] = url.split('?');
   const parts = path.slice(1).split('/');
-  const params = new URLSearchParams(search | '');
+  const searchString = search || '';
+  const params = new URLSearchParams(searchString);
   const query = Object.fromEntries(params.entries());
 
   return { parts, query };
